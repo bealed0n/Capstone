8ce19d76fd3463dc9a937d7b7517e68aad155eda
bealed0n <benj.loyola@duocuc.cl>
2024-10-17 23:40:16 -0300

    cambios menores en perfil de clientes

--- frontend-Ign-Tattoo-App/components/EditScreenInfo.tsx
+++ frontend-Ign-Tattoo-App/components/EditScreenInfo.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { StyleSheet, } from 'react-native';
+import { StyleSheet, Image } from 'react-native';
 
 import { ExternalLink } from './ExternalLink';
 import { MonoText } from './StyledText';
@@ -15,32 +15,16 @@ export default function EditScreenInfo({ path }: { path: string }) {
           style={styles.getStartedText}
           lightColor="rgba(0,0,0,0.8)"
           darkColor="rgba(255,255,255,0.8)">
-          Open up the code for this screen:
+          When you get a tattoo it will appear here!
         </Text>
-
-        <View
-          style={[styles.codeHighlightContainer, styles.homeScreenFilename]}
-          darkColor="rgba(255,255,255,0.05)"
-          lightColor="rgba(0,0,0,0.05)">
-          <MonoText>{path}</MonoText>
-        </View>
-
         <Text
           style={styles.getStartedText}
           lightColor="rgba(0,0,0,0.8)"
-          darkColor="rgba(255,255,255,0.8)">
-          Change any of the text, save the file, and your app will automatically update.
+          darkColor="rgba(255,255,255,0.8)"
+          className='mb-3'>
+          It seems that you have not had a tattoo yet
         </Text>
-      </View>
-
-      <View style={styles.helpContainer}>
-        <ExternalLink
-          style={styles.helpLink}
-          href="https://docs.expo.io/get-started/create-a-new-app/#opening-the-app-on-your-phonetablet">
-          <Text style={styles.helpLinkText} lightColor={Colors.light.tint}>
-            Tap here if your app doesn't automatically update after making changes
-          </Text>
-        </ExternalLink>
+        <Image source={require('../assets/images/notfound.png')} style={{ width: 200, height: 200 }} />
       </View>
     </View>
   );
--- frontend-Ign-Tattoo-App/components/UserProfile.tsx
+++ frontend-Ign-Tattoo-App/components/UserProfile.tsx
@@ -1,16 +1,23 @@
-import React from 'react';
+import React, { useContext } from 'react';
 import { TouchableOpacity } from 'react-native';
 import { View, Text } from '@/components/Themed'; // Asegúrate de que este componente esté bien configurado
 import { useNavigation } from '@react-navigation/native';
 import { Ionicons } from '@expo/vector-icons';
 import { Href, router } from 'expo-router';
 import EditScreenInfo from './EditScreenInfo';
+import { UserContext } from '@/app/context/userContext';
 
 export default function UserProfile() {
     const navigation = useNavigation();
+    const { user } = useContext(UserContext);
 
     return (
         <View>
+            <View className='ml-5'>
+                <Text className='font-bold text-lg'>
+                    {user?.username ?? 'No encontrado'}
+                </Text>
+            </View>
             <View className='mb-1'>
                 <Text className='font-bold text-xl text-center mb-4'>
                     Dashboard
@@ -40,7 +47,7 @@ export default function UserProfile() {
             <View className=" mt-4 mb-2 h-0.5 border-t-0 bg-neutral-200 dark:bg-white/10" />
             <View>
                 <Text className='font-bold text-center text-lg'>
-                    Mis tatuajes
+                    My tattoos
                 </Text>
                 <EditScreenInfo path="/screens/UserProfile.tsx" />
 
